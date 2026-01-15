a2944b115325a17c1ee7466d7a57df09bca1780f
Edwin Aguiar <fenixlife1978@gmail.com>
2026-01-15 18:36:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6594642834-e6953",
+  "appId": "1:951922820406:web:0c9a2264b559c240869f97",
+  "apiKey": "AIzaSyAVsMbGyBIIRrsJ_s2tJocUMpKv0LR2pgM",
+  "authDomain": "studio-6594642834-e6953.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "951922820406"
+};
