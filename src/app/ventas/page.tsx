f78f29b0181f7b8a2c9fa7f74f84f279e259eb2b
Edwin Aguiar <fenixlife1978@gmail.com>
2026-01-15 20:22:33 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    Link is not defin

--- src/app/ventas/page.tsx
+++ src/app/ventas/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useMemo } from 'react'
 import { Plus, Search, ShoppingCart, DollarSign, Trash2 } from 'lucide-react'
+import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useToast } from '@/hooks/use-toast'
